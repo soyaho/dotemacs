a704008ecf1c2942bee3b6922cf8f35ba0939735
isamu-hosoya <isamu.hosoya@gree.net>
2016-07-07 11:24:20 +0900

    setq exec-path

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; PATHを通す
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; cask
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
